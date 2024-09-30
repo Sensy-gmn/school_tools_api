7a8cc8cc11c38cd15499564afbbe9fa1d5ccb0b7
Sensy-gmn <alexandre26goumain@gmail.com>
2024-09-30 15:50:36 +0200

    port to 3000

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const noteRoutes = require("./routes/noteRoutes");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
